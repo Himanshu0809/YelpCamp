91b0ddfcb3434933a6ff9080b4a9f64aef2bbbb8
Himanshu <guptahimanshu479@gmail.com>
2020-01-20 09:06:35 +0530

    Yelpcamp slugs- updated model

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,21 +1,24 @@
 var mongoose = require("mongoose");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: "Campground name cannot be blank."
+    },
     price: String,
     image: String,
-    imageId:String,
-    createdAt: { type: Date, default: Date.now }, 
+    imageId: String,
+    createdAt: { type: Date, default: Date.now },
     // location: String,
     // lat: Number,
     // lng: Number,
     description: String,
-    author:{
-        id:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User"
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
         },
-        username:String
+        username: String
     },
     comments: [
         {
@@ -38,8 +41,56 @@ var campgroundSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         }
-    ]
+    ],
+    slug: {
+        type: String,
+        unique: true
+    }
+});
+
+// add a slug before the campground gets saved to the database
+campgroundSchema.pre('save', async function (next) {
+    try {
+        // check if a new campground is being saved, or if the campground name is being modified
+        if (this.isNew || this.isModified("name")) {
+            this.slug = await generateUniqueSlug(this._id, this.name);
+        }
+        next();
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
-  
+
+async function generateUniqueSlug(id, campgroundName, slug) {
+    try {
+        // generate the initial slug
+        if (!slug) {
+            slug = slugify(campgroundName);
+        }
+        // check if a campground with the slug already exists
+        var campground = await Campground.findOne({ slug: slug });
+        // check if a campground was found or if the found campground is the current campground
+        if (!campground || campground._id.equals(id)) {
+            return slug;
+        }
+        // if not unique, generate a new slug
+        var newSlug = slugify(campgroundName);
+        // check again by calling the function recursively
+        return await generateUniqueSlug(id, campgroundName, newSlug);
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+function slugify(text) {
+    var slug = text.toString().toLowerCase()
+        .replace(/\s+/g, '-')        // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')    // Remove all non-word chars
+        .replace(/\-\-+/g, '-')      // Replace multiple - with single -
+        .replace(/^-+/, '')          // Trim - from start of text
+        .replace(/-+$/, '')          // Trim - from end of text
+        .substring(0, 75);           // Trim at 75 characters
+    return slug + "-" + Math.floor(1000 + Math.random() * 9000);  // Add 4 random digits to improve uniqueness
+}
